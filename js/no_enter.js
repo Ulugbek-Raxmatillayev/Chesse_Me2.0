dc6322125353d4c85af5524ec3d986ad644d958d
Uktamuzb <uktamuzoqov2@gmail.com>
2024-05-21 12:23:49 +0500

    modall go

--- js/no_enter.js
+++ js/no_enter.js
@@ -8,3 +8,25 @@ ckickMe.addEventListener('click', () => {
     popular.classList.toggle('d-none')
     tavsiv.classList.toggle('d-none')
 })
+ 
+let btnModal = document.querySelectorAll('.retsept__korzinka button') 
+let modal = document.querySelector('.modal__container') 
+let clos = document.querySelector('.modal__title') 
+
+btnModal.forEach(element => {
+    modalMenu(element, modal) 
+});
+
+modalMenu(clos, modal) 
+
+function modalMenu(btn, togl) {
+    btn.addEventListener('click', () =>{
+        togl.classList.toggle('d-none')
+    })
+}
+
+modal.addEventListener('click', (e) =>{
+    if(e.target == modal){
+        modal.classList.toggle('d-none')
+    }
+})
