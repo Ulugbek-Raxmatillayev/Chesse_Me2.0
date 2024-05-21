8595458723d7393a688d2e580c6dc7e5ead9e377
Uktamuzb <uktamuzoqov2@gmail.com>
2024-05-21 16:52:11 +0500

    modal

--- js/no_enter.js
+++ js/no_enter.js
@@ -12,26 +12,31 @@ ckickMe.addEventListener('click', () => {
 // btn esho more finish///////////////////////////////////////////////////////
 
 // CLOSE modalll starrt ///////////////////////////////////////////////////////
-let btnModal = document.querySelectorAll('.retsept__korzinka button') 
-let modal = document.querySelector('.modal__container') 
-let clos = document.querySelector('.modal__title') 
+let btnModal = document.querySelectorAll('.retsept__korzinka button')
+let modal = document.querySelector('.modal__container')
+let clos = document.querySelector('.modal__title')
+let body = document.querySelector('body')
 
 btnModal.forEach(element => {
-    modalMenu(element, modal) 
+    modalMenu(element, modal)
 });
 
-modalMenu(clos, modal) 
+modalMenu(clos, modal)
 
 function modalMenu(btn, togl) {
-    btn.addEventListener('click', () =>{
+    btn.addEventListener('click', () => {
         togl.classList.toggle('d-none')
+        body.classList.toggle('fixed')
     })
 }
 
-modal.addEventListener('click', (e) =>{
-    if(e.target == modal){
+modal.addEventListener('click', (e) => {
+    if (e.target == modal) {
         modal.classList.toggle('d-none')
+        body.classList.toggle('fixed')
     }
 })
+
+
 
 // CLOSE modalll finish //////////////////////////////////////////////
